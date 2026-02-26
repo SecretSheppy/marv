5d992adc236ce648a4ebfee2c4f6f3a1d9685f2a
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2026-02-26 15:35:47 +0000

    refactor: add .marv.yml overwrite prevention

--- internal/cmds/init.go
+++ internal/cmds/init.go
@@ -9,17 +9,23 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+const marvYml = ".marv.yml"
+
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "initialises a new default marv.yml file",
 	Long:  "initialises a new default marv.yml file in the current working directory",
 	Run: func(cmd *cobra.Command, args []string) {
+		if f, _ := os.Stat(marvYml); f != nil {
+			fmt.Println(".marv.yml already exists in this directory")
+			os.Exit(0)
+		}
 		marshal, err := yaml.Marshal(config.Init())
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 			os.Exit(1)
 		}
-		if err := os.WriteFile(".marv.yml", marshal, 0644); err != nil {
+		if err := os.WriteFile(marvYml, marshal, 0644); err != nil {
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 			os.Exit(1)
 		}
