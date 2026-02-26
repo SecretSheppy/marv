8a5178d6832f3a770ff99af7ab01a5805b3d8198
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2026-02-26 15:51:25 +0000

    feat: add yml loading and merge with optional flags

--- internal/cmds/root.go
+++ internal/cmds/root.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/SecretSheppy/marv/internal/config"
 	"github.com/SecretSheppy/marv/internal/marvinfo"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
+
+const defaultPort = 8080
 
 var (
 	port                     int
-	src, review              string
+	src, review, configFile  string
 	toolRunner, fileWatchers bool
 
 	rootCmd = &cobra.Command{
@@ -20,10 +24,45 @@ var (
 review of mutations through visualisations - it can be used 'as is' or can be integrated into a
 third party application to streamline review processes`,
 		Run: func(cmd *cobra.Command, args []string) {
+			yml, err := os.ReadFile(configFile)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+
+			_, err = mergeYmlFlagConfigs(yml)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+
 			// TODO: start main application server here
 		},
 	}
 )
+
+func mergeYmlFlagConfigs(yml []byte) (*config.Config, error) {
+	cfg := &config.Config{}
+	if err := yaml.Unmarshal(yml, cfg); err != nil {
+		return nil, err
+	}
+	if port != defaultPort {
+		cfg.Web.Port = port
+	}
+	if src != "" {
+		cfg.Paths.Sources = src
+	}
+	if review != "" {
+		cfg.Paths.Reviews = review
+	}
+	if toolRunner {
+		cfg.Features.ToolRunner = true
+	}
+	if fileWatchers {
+		cfg.Features.FileWatchers = true
+	}
+	return cfg, nil
+}
 
 func Execute() {
 	rootCmd.Version = marvinfo.Get().Version
@@ -32,6 +71,7 @@ func Execute() {
 	rootCmd.Flags().IntVarP(&port, "port", "p", 8080, "port to listen on")
 	rootCmd.Flags().StringVarP(&src, "src", "s", "", "source files directory")
 	rootCmd.Flags().StringVarP(&review, "review", "r", "", "review output directory")
+	rootCmd.Flags().StringVarP(&configFile, "config", "c", ".marv.yml", ".marv.yml file path")
 	rootCmd.Flags().BoolVarP(&toolRunner, "enable-tool-runner", "t", false, "enable tool runner")
 	rootCmd.Flags().BoolVarP(&fileWatchers, "enable-watchers", "w", false, "enable file watchers")
 
