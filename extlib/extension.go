8155fe4679860903f28e6897f510f1aa7fd34979
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2026-03-06 12:15:19 +0000

    feat: add Meta struct for extension metadata

--- extlib/extension.go
+++ extlib/extension.go
@@ -1,7 +1,18 @@
 package extlib
 
+type Meta struct {
+	Name   string
+	Lang   string
+	TSLang string // TODO: should be replaced with type of tree sitter language, haven't yet decided which tree sitter package to use
+	URL    string
+	RunStr string
+}
+
 // Extension defines what methods an extension must have in order to interact with the marv system.
 type Extension interface {
+	Meta() *Meta
 }
 
 // TODO: resources static dir
+// TODO: formatted mutations
+// TODO: toggle bits of marv specification/functionality
