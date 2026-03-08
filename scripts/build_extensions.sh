323ff40fe3e458d0443bd84e9219540742d344de
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2026-03-08 16:24:05 +0000

    feat: add build extensions script

--- scripts/build_extensions.sh
+++ scripts/build_extensions.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+for dir in */; do
+    dirname=$(basename "$dir")
+    go_file="$dir$dirname.go"
 
-# TODO:
-for i...
-go build -buildmode=plugin -o plugin.so plugin.go
+    if [[ -f "$go_file" ]]; then
+        go build -buildmode=plugin -o "$dir$dirname.so" "$go_file"
+
+        if [[ $? -eq 0 ]]; then
+            echo "Compiled $go_file to $dirname.so"
+        else
+            echo "Failed to compile $go_file"
+        fi
+    else
+        echo "Go file $go_file does not exist"
+    fi
+done
