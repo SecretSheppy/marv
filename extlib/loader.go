cddfd0d3c3cad86c990738ee243f8b38f7062c48
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2026-03-08 16:39:58 +0000

    feat: add single extension loader

--- extlib/loader.go
+++ extlib/loader.go
@@ -1,1 +1,30 @@
 package extlib
+
+import (
+	"plugin"
+)
+
+func Load(p string) ([]*Extension, error) {
+	exts := make([]*Extension, 0)
+
+	// TODO: loop
+	plg, err := plugin.Open("")
+	if err != nil {
+		return nil, err
+	}
+
+	v, err := plg.Lookup("Ext")
+	if err != nil {
+		return nil, err
+	}
+
+	ext, ok := v.(*Extension)
+	if !ok {
+		return nil, ErrNoExtDeclaration
+	}
+
+	exts = append(exts, ext)
+	// TODO: end loop
+
+	return exts, nil
+}
