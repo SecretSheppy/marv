e9739c2d5e747249379e26d33ddc272d00416c3f
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2026-03-19 21:46:58 +0000

    refactor: remove pitest from Frameworks

--- fws/fws.go
+++ fws/fws.go
@@ -3,12 +3,10 @@ package fws
 import (
 	"github.com/SecretSheppy/marv/fwlib"
 	"github.com/SecretSheppy/marv/fws/mutest_rs"
-	"github.com/SecretSheppy/marv/fws/pitest"
 )
 
 func Frameworks() []fwlib.Framework {
 	return []fwlib.Framework{
 		&mutest_rs.MutestRS{},
-		&pitest.Pitest{},
 	}
 }
