0af86a2c1a251f91d99af1f430b3c1fc7c814d86
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2026-04-05 16:43:14 +0100

    feat: add status icons

--- internal/mutations/mutations.go
+++ internal/mutations/mutations.go
@@ -1,6 +1,7 @@
 package mutations
 
 import (
+	"fmt"
 	"sort"
 
 	"github.com/google/uuid"
@@ -16,6 +17,35 @@ const (
 	Timeout    Status = "TIMEOUT"
 	NoCoverage Status = "NO_COVERAGE"
 )
+
+var statusPaths = map[Status]string{
+	Killed: "<path d=\"M256 512a256 256 0 1 1 0-512 256 256 0 1 1 0 512zM374 145.7c-10.7-7.8-25.7-5.4-33.5 5.3L221" +
+		".1315.2 169 263.1c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l72 72c5 5 11.8 7.5 18.8 7s13.4-4.1 17.5-9.8L3" +
+		"79.3 179.2c7.8-10.7 5.4-25.7-5.3-33.5z\"/>",
+	Survived: "<path d=\"M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zM167 167c9.4-9.4 24.6-9.4 33.9 0l55 55 5" +
+		"5-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55" +
+		"-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9z\"/>",
+	Crashed: "<path d=\"M256 512a256 256 0 1 1 0-512 256 256 0 1 1 0 512zm0-192a32 32 0 1 0 0 64 32 32 0 1 0 0-64z" +
+		"m0-192c-18.2 0-32.7 15.5-31.4 33.7l7.4 104c.9 12.6 11.4 22.3 23.9 22.3 12.6 0 23-9.7 23.9-22.3l7.4-104c1." +
+		"3-18.2-13.1-33.7-31.4-33.7z\"/>",
+	Timeout: "<path d=\"M256 0a256 256 0 1 1 0 512 256 256 0 1 1 0-512zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 " +
+		"7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z\"/>",
+	NoCoverage: "<path d=\"M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zm0-336c-17.7 0-32 14.3-32 32 0 13.3-10" +
+		".7 24-24 24s-24-10.7-24-24c0-44.2 35.8-80 80-80s80 35.8 80 80c0 47.2-36 67.2-56 74.5l0 3.8c0 13.3-10.7 24" +
+		"-24 24s-24-10.7-24-24l0-8.1c0-20.5 14.8-35.2 30.1-40.2 6.4-2.1 13.2-5.5 18.2-10.3 4.3-4.2 7.7-10 7.7-19.6" +
+		" 0-17.7-14.3-32-32-32zM224 368a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z\"/>",
+}
+
+// Icon returns the icon belonging to the respective Status.
+func (s Status) Icon(colour string) string {
+	svgPath, exists := statusPaths[s]
+	if !exists {
+		svgPath = statusPaths[NoCoverage]
+	}
+	return fmt.Sprintf("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\" fill=\"%s\"><!--!"+
+		"Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/lice"+
+		"nse/free Copyright 2026 Fonticons, Inc.-->%s</svg>", colour, svgPath)
+}
 
 // Range holds a line and char index.
 type Range struct {
